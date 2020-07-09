610ea59fadef0786c4af3ca1ce3f1b07decb6efb
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-09 18:01:11 +0500

    add start function to start stopwatch

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("stopwatch");
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+var interval;
+
+function timer() {
+    msec++
+    msecHeading.innerHTML = msec 
+
+    if(msec >= 100) {
+        sec++ 
+        secHeading.innerHTML =  sec + ':' 
+        msec = 0
+    }
+    else if(sec >= 60) {
+        min++
+        minHeading.innerHTML = min + ':'
+        sec = 0
+    }
+}
+
+function start() {
+    interval = setInterval(timer, 10)
+}
